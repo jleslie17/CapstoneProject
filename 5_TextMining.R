4f2c89491a8dc2e1c1551a77b1c55b15de9e9e41
jleslie17 <jleslie17@yahoo.com>
2015-11-14 19:18:07 +0000

    File 5 with keyword list function

--- 5_TextMining.R
+++ 5_TextMining.R
@@ -4,7 +4,37 @@ library(SnowballC)
 CorpusBest <- VCorpus(VectorSource(BestRevsGathered$Texts))
 CorpusWorst <- VCorpus(VectorSource(WorstRevsGathered$Texts))
 
+CorpusWorstTrans <- tm_map(CorpusWorst, stripWhitespace)
+CorpusWorstTrans <- tm_map(CorpusWorstTrans, content_transformer(tolower))
+CorpusWorstTrans <- tm_map(CorpusWorstTrans, removeWords, stopwords("english"))
+CorpusWorstTrans <- tm_map(CorpusWorstTrans, stemDocument)
 
+tdmWorst <- TermDocumentMatrix(CorpusWorstTrans)
+tdmWorstFreq <- findFreqTerms(tdmWorst, 50, Inf) #Character vector
+
+##Function to extract keywords from a corpus
+FindKeyWords <- function(inputCorp){
+        dtmTemp <- DocumentTermMatrix(inputCorp)
+        dtmTemp <- removeSparseTerms(dtmTemp,0.4)
+        dtmFreqTemp <- findFreqTerms(dtmTemp, 5)
+        return(dtmFreqTemp)
+}
+##Set Keyword list to empty list
+KeywordListWorst <- list()
+##Loop to get the keywords from each business in the worst list
+for(i in unique(WorstRevsGathered$business_id)){
+        CorpusTemp <- VCorpus(VectorSource(WorstRevsGathered$Texts[
+                WorstRevsGathered$business_id == i]))
+        CorpusTemp <- tm_map(CorpusTemp, stripWhitespace)
+        CorpusTemp <- tm_map(CorpusTemp, content_transformer(tolower))
+        CorpusTemp <- tm_map(CorpusTemp, removeWords, stopwords("english"))
+        CorpusTemp <- tm_map(CorpusTemp, stemDocument)
+        WordsTemp <- FindKeyWords(CorpusTemp)
+        KeywordListWorst <- append(KeywordListWorst, list(WordsTemp))
+}
+
+WorstWordsDF[1,1]
+unique(KeywordListBest)
 
 TMTestVector <- WorstRevsGathered[WorstRevsGathered$business_id == 
                                           "27ADmOieSUZfbiU15als7w",]
@@ -24,13 +54,24 @@ CorpusTestTrans <- tm_map(CorpusTestTrans, stemDocument)
 
 #could also perform synonym analysis and remove sparse terms. See ref
 
-
 #TermDocumentMatrix
 tdm <- TermDocumentMatrix(CorpusTestTrans)
-tdmFreq <- findFreqTerms(tdm, 50, Inf) #Character vector
-str(tdm)
-tdm[, "also"]
-sort(tdmFreq)
+tdmSparseRem <- removeSparseTerms(tdm,0.4)
+tdmFreq <- findFreqTerms(tdmSparseRem, 5, Inf) #Character vector
+tdmFreq
+Keywords <- list()
+count <- 0
+FindKeyWords <- function(inputCorp){
+        dtmTemp <- DocumentTermMatrix(inputCorp)
+        dtmTemp <- removeSparseTerms(dtmTemp,0.4)
+        dtmFreqTemp <- findFreqTerms(dtmTemp, 5)
+        return(dtmFreqTemp)
+}
+FindKeyWords(CorpusTestTrans)
+Keywords <- c(Keywords, tdmFreq)
+Keywords <- c
+testList <- list(tdm, tdmSparseRem)
+testList <- append(testList,list(tdm))
 
 
 ###This is how I'll do it!
@@ -43,75 +84,18 @@ for(i in row.names(test)) {
 testSushi <- test['sushi',]
 SushiNames <- names(testSushi)
 summary(testSushi)
+
+test <- inspect(tdm[tdmFreq, ])
 x <- as.numeric(SushiNames)
-y <- as.numeric(testSushi)
-plot(x,y) 
-abline(lm(y~x))
-lm(y~x)$coef[2]
-        
-
-
-
-inspect(tdm[tdmFreq[1],])
-inspect(tdm['also',])
-inspect(tdm['sushi',])
-
-y=Terms(tdm[tdmFreq[1],])
-x=Docs(tdm)
-plot(, y)
-
-Docs(tdm)
-
-
-
-
-
-
-
-m3 <- as.matrix(tdm$dimnames)
-
-tdm$dimnames$Terms[1:10, tdmFreq]
-
-tdm$dimnames$Docs == "55"
-tdm[tdm$dimnames$Docs == "55"]
-findFreqTerms(tdm[tdm$dimnames$Docs == "55"], 20)
-tdm[6]
-
-dtm <- DocumentTermMatrix(CorpusTestTrans)
-dtm2 <- removeSparseTerms(dtm, 0.4)
-inspect(dtm2)
-
-
-findFreqTerms(dtm, 20, 100)
-CorpusTestTrans[[2]]
-
-
-dtm$dimnames$Docs
-findFreqTerms(dtm[dtm$dimnames$Docs == "1"], 20, 100)
-dtm[1]
-str(dtm)
-inspect(removeSparseTerms(dtm, 0.4))
-termFreq()
-findFreqTerms()
-dtm$dimnames$Docs
-
-m <- as.matrix(tdm)
-v <- sort(rowSums(m), decreasing = T)
-head(v, 10)
-v2 <- sort(m, decreasing = T)
-head(v2, 10)
-
-findFreqFunction <- function(x) {
-        findFreqTerms(x, 20, Inf)
+plot(x,y,type = 'n')
+for(i in tdmFreq){
+        termTrend <- test[i,]
+        y <- as.numeric(termTrend)
+        abline(lm(y~x))
+        print(lm(y~x)$coef[2])
 }
 
-tdm[1:10, findFreqFunction]
-
-findAssocs(tdm, "qualiti", 0.5)
-inspect(removeSparseTerms(tdm, 0.4))
-DictDTMTest <- inspect(DocumentTermMatrix(CorpusTestTrans, 
-                           list(dictionary = c('sushi', 'slow','rude','qualiti'))))
-str(DictDTMTest)
-stopwords('en')
-test <- as.matrix(DictDTMTest)
-test[1,]
+##Find associations
+word = "disappoint"
+dictionary = c("poor", "servic","rude")
+findAssocs(tdm, dictionary, 0.5)
